ffb36123b5fe3d2c910c68e2c0cc947bc3bdef4f
kimkyunghwan__ <un5556@naver.com>
2020-08-25 15:43:31 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 
-function App() {
-  return (
-    <div className="gray-backgournd">
-      <img src={logo} lat="logo" />
-      <h2>Let's Develop</h2>
-    </div>
-  );
+const custormers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "EBRA",
+    birthday: "900914",
+    gender: "남자",
+    job: "무직",
+  },
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/2",
+    name: "MESSI",
+    birthday: "870914",
+    gender: "남자",
+    job: "무직",
+  },
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/3",
+    name: "COCU",
+    birthday: "760914",
+    gender: "남자",
+    job: "축구선수",
+  },
+];
+
+//위 부분이 데이터베이스에 스테이트로
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          //customers배열을 c로 순회 travers한다
+          custormers.map((c) => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                iamge={c.iamge}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              ></Customer>
+            );
+          })
+        }
+        ;
+      </div>
+    );
+  }
 }
 
 export default App;
