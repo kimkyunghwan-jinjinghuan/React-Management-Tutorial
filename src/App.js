e62e994503cfe3f743c3185b7b6e443f9df15e60
kimkyunghwan__ <un5556@naver.com>
2020-09-02 14:53:50 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import "./App.css";
 import React, { Component } from "react";
 import Customer from "./components/Customer";
+
+import "./App.css";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowx: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
 
 const custormers = [
   {
@@ -12,7 +31,7 @@ const custormers = [
     job: "무직",
   },
   {
-    id: 1,
+    id: 2,
     image: "https://placeimg.com/64/64/2",
     name: "MESSI",
     birthday: "870914",
@@ -20,7 +39,7 @@ const custormers = [
     job: "무직",
   },
   {
-    id: 1,
+    id: 3,
     image: "https://placeimg.com/64/64/3",
     name: "COCU",
     birthday: "760914",
@@ -28,33 +47,47 @@ const custormers = [
     job: "축구선수",
   },
 ];
-
 //위 부분이 데이터베이스에 스테이트로
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
+    // console.log(classes);
+
     return (
-      <div>
-        {
-          //customers배열을 c로 순회 travers한다
-          custormers.map((c) => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                iamge={c.iamge}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              ></Customer>
-            );
-          })
-        }
-        ;
-      </div>
+      <Paper>
+        <Table className={classes.root}>
+          <TableHead className={classes.table}>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+          <TableBody>
+            {
+              //customers배열을 c로 순회 travers한다
+              custormers.map((c) => {
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  ></Customer>
+                );
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
+// export default App;
